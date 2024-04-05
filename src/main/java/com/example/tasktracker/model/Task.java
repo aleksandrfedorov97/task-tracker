cb7228b78aefc3beeaa58517dc2c3d56d533339d
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-04-05 09:16:04 +0300

    feat: annotations for model Task

--- src/main/java/com/example/tasktracker/model/Task.java
+++ src/main/java/com/example/tasktracker/model/Task.java
@@ -1,14 +1,23 @@
 package com.example.tasktracker.model;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "tasks")
 public class Task {
+    @Id
     private String id;
     private String name;
     private String description;
@@ -16,10 +25,16 @@ public class Task {
     private Instant updatedAt;
     private TaskStatus status;
     private String authorId;
+
+    @ReadOnlyProperty
     private User author;
     private String assignedId;
+
+    @ReadOnlyProperty
     private User assignee;
-    private Set<String> observerIds;
+    private Set<String> observerIds = new HashSet<>();
+
+    @ReadOnlyProperty
     private Set<User> observers;
 
     public enum TaskStatus {
