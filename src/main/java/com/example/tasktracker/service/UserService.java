c98eede196a8e761727c6b761892be38b753bb64
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-21 08:40:23 +0300

    feat: UserService.java (findByUsername), using passwordEncoder

--- src/main/java/com/example/tasktracker/service/UserService.java
+++ src/main/java/com/example/tasktracker/service/UserService.java
@@ -1,8 +1,9 @@
 package com.example.tasktracker.service;
 
+import com.example.tasktracker.model.RoleType;
 import com.example.tasktracker.model.User;
 import com.example.tasktracker.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
@@ -12,9 +13,24 @@ import java.util.Set;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(final UserRepository userRepository, final PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+        User user = User.builder()
+                .id(UUID.fromString("123e4567-e89b-12d3-a456-426655440000").toString())
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .email("admin@mail.com")
+                .roles(Set.of(RoleType.ROLE_MANAGER))
+                .build();
+
+        findById(user.getId()).defaultIfEmpty(userRepository.save(user).block());
+    }
 
     public Flux<User> findAll() {
         return userRepository.findAll();
@@ -27,6 +43,9 @@ public class UserService {
     public Mono<User> findById(String id) {
         return userRepository.findById(id);
     }
+    public Mono<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 
     public Mono<Boolean> existsById(String id) {
         return userRepository.existsById(id);
@@ -34,6 +53,7 @@ public class UserService {
 
     public Mono<User> create(User user) {
         user.setId(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
