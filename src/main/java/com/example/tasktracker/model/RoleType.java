83d442d278643720099d6ad6f4a1f703f14abd2e
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-21 08:36:04 +0300

    feat: model User, added fields (password, roles)

---
+++ src/main/java/com/example/tasktracker/model/RoleType.java
@@ -0,0 +1,6 @@
+package com.example.tasktracker.model;
+
+public enum RoleType {
+    ROLE_USER,
+    ROLE_MANAGER
+}
--- src/main/java/com/example/tasktracker/model/User.java
+++ src/main/java/com/example/tasktracker/model/User.java
@@ -1,11 +1,17 @@
 package com.example.tasktracker.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,5 +21,9 @@ public class User {
     @Id
     private String id;
     private String username;
+    private String password;
     private String email;
+
+    @Field("roles")
+    private Set<RoleType> roles = new HashSet<>();
 }
